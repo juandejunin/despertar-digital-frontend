3e516c9a8512c4d7bc4cd4b284dd71f4496324ff
juandejunin <juandejunin75@gmail.com>
2025-10-30 12:37:23 +0100

    nueva configuracion: se cambia el astro config msj para funcioar como srr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
-// https://astro.build/config
 export default defineConfig({
-   integrations: [preact()],
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  integrations: [preact(), tailwind()],
+  output: 'server',
+  adapter: node({ mode: 'standalone' }),
 });
